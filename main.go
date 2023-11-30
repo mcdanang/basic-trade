e506d9bbbe9ad178187b2ce2b16be03992ba7991
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-11-30 22:18:58 +0700

    update port config

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"basic-trade/database"
 	"basic-trade/routers"
+	"os"
 )
 
 func main() {
-	var PORT = ":8080"
+	var PORT = os.Getenv("PORT")
+
+	if PORT == "" {
+		PORT = "8080" // Default port if PORT environment variable is not set
+	}
 
 	database.StartDB()
 
-	routers.StartServer().Run(PORT)
+	routers.StartServer().Run(":" + PORT)
 }
